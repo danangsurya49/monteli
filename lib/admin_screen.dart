b9e068bf12131066cd5d49c338b97aadc2040ba6
L2Luck <rama27462@gmail.com>
2025-06-04 17:27:51 +0700

    admin2

--- lib/admin_screen.dart
+++ lib/admin_screen.dart
@@ -10,6 +10,8 @@ class AdminScreen extends StatefulWidget {
 class _AdminScreenState extends State<AdminScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+    );
   }
 }
