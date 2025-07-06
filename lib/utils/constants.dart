7a2c8ed936785864bad4136cecc0c105bcc4ba4f
L2Luck <rama27462@gmail.com>
2025-07-06 14:05:48 +0700

    mengubah/ganti database

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 // lib/utils/constants.dart
 
 // GANTI DENGAN URL DAN ANON KEY DARI PROYEK SUPABASE ANDA
-const String supabaseUrl = 'https://ideganazjrjppftfmhwh.supabase.co';
-const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlkZWdhbmF6anJqcHBmdGZtaHdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3MDMyODksImV4cCI6MjA2NzI3OTI4OX0.HcknmvYrUo8sUxAGuSThz5g8zS70MA7XVKUMoGk6t8s';
+const String supabaseUrl = 'https://djxevsdivoaoeoxpztwu.supabase.co';
+const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqeGV2c2Rpdm9hb2VveHB6dHd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3Nzk4NDMsImV4cCI6MjA2NzM1NTg0M30.kXSJUDnU-rSU5_UwaR8TphclIbas06HKkRMVfnboumw';
