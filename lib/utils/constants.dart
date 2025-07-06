a6b659e56ef8d20feec506e18b490e7ee149443f
L2Luck <rama27462@gmail.com>
2025-07-06 10:05:38 +0700

    pubsec dependensi ke supabase

---
+++ lib/utils/constants.dart
@@ -0,0 +1,5 @@
+// lib/utils/constants.dart
+
+// GANTI DENGAN URL DAN ANON KEY DARI PROYEK SUPABASE ANDA
+const String supabaseUrl = 'https://ideganazjrjppftfmhwh.supabase.co';
+const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlkZWdhbmF6anJqcHBmdGZtaHdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3MDMyODksImV4cCI6MjA2NzI3OTI4OX0.HcknmvYrUo8sUxAGuSThz5g8zS70MA7XVKUMoGk6t8s';
